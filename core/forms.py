e84ff2e5a268c5129e5edaee2e4bef5e480d0bbe
Techoffice10 <info@techoffice.ca>
2024-11-25 17:57:25 -0500

    25th EOD Updates

--- core/forms.py
+++ core/forms.py
@@ -8,8 +8,6 @@ class UserInfoForm(forms.ModelForm):
         fields = ['user_name', 'full_name', 'phone_no', 'email', 'node', 'branch', 'ext_no', 'port_ip', 
                   'password', 'power_broker_id', 'applied_rating_id', 'user_creation_date', 'suspended_date', 'status']
 
-
-
     # Optionally customize the widgets
     user_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter user name'}))
     full_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter full name'}))
@@ -26,7 +24,15 @@ class UserInfoForm(forms.ModelForm):
     suspended_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'placeholder': 'Enter suspended date'}))
     status = forms.ChoiceField(choices=[('active', 'Active'), ('inactive', 'Inactive')])
 
+    # Make sure all fields are editable after a search operation or on form re-render
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Ensure fields are not set to read-only or disabled after a search
+        for field_name, field in self.fields.items():
+            field.disabled = False  # Explicitly set the field to be editable
+            field.widget.attrs.pop('readonly', None)  # Remove readonly if set
 
+# If you have another form, like YourForm, this should be left as is, assuming it's a different use case.
 class YourForm(forms.Form):
     STATUS_CHOICES = [
         ('active', 'Active'),
